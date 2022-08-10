b6033e29cec9a5cc773e7abfe56ae618e78ff77b
Navwojah-Ariko_Eloho <arikoeloho1996@gmail.com>
2022-08-10 13:07:33 +0100

    fix: enable input and storage of operand

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,4 @@
+//create functions for each of the operators.
 function addOperator(a,b) {
     return(a+b) 
 }
@@ -14,28 +15,76 @@ function divideOperator(a,b) {
     return(a/b) 
 }
 
+//the operate function will call any of the following operator functions when the corresponding button is clicked
 function operate (m,x,n) {
-    if (x="+") {
-        addOperator(m,n)
+    if (x==="+") {
+         return addOperator(m,n)
     }
-    else if  (x="-") {
-        subtractOperator(m,n)
+    else if  (x==="-") {
+        return subtractOperator(m,n)
     }
-    else if  (x="*") {
-        multiplyOperator(m,n)
+    else if  (x==="*") {
+         return multiplyOperator(m,n)
     }
-     else if  (x="/") {
-        divideOperator(m,n)
+     else if  (x==="/") {
+        return divideOperator(m,n)
     }
 }
 
+//prevents the user from entering the decimal point more than once for a particular operand
+function decimalDisable() {
+    let decimalBtn=document.querySelector("#decimalBtn")
+    if (display.innerText.includes('.')==true) {
+    decimalBtn.disabled=true}
+    else {
+        decimalBtn.disabled=false
+    }
+}
 
+//populates the display div with numbers when clicked, also calls the decimalDisable button
 function populateDisplay () {
     let display=document.querySelector('.display');
-    let numBtn=Array.from(document.querySelectorAll('.numberBtn'));
-    let displayValue= numBtn.map(btn => { btn.addEventListener('click', evt => display.innerText += evt.target.innerText)} )}
+    let numBtn=document.querySelectorAll('.numberBtn');
+    numBtn.forEach(button => {
+        button.addEventListener('click', evt => {
+            display.innerText += evt.target.innerText;
+            decimalDisable()
+        })
+    })
+}
 
-    populateDisplay()
+let lhs;
+let rhs;
+let op;
+document.querySelectorAll('.operator').forEach((btn) =>
+btn.addEventListener('click',() => {
+    const operatorMap={
+        add:'+',
+        subtract:'-',
+        multiply:'*',
+        divide:'/'
+    };
+    const operation=operatorMap[btn.id];
+    const display=document.querySelector('.display');
+    const currentText=display.innerText;
+
+    if (btn.id==='equals'){
+        console.log(Number(lhs),op,Number(currentText));
+        const result=operate(Number(lhs),op,Number(currentText));
+        console.log('result', result);
+
+        lhs=op=undefined;
+        return display.innerText=result
+    }
+    lhs=currentText;
+    op=operation;
+    display.innerText ='';
+    console.log('lhs', lhs);
+    console.log('operation is', operation);
+
+}));
+
+populateDisplay();
 
 let display=document.querySelector('.display');
 let allClear=document.querySelector("#allClear");
@@ -47,11 +96,10 @@ let clear =document.querySelector("#clear");
 clear.addEventListener('click', ()=> { 
     display.innerText=display.innerText.slice(0,-1)
 });
-//
-let decimalBtn=document.querySelector("#decimalBtn")
-if (display.innerText.includes()==true) {
-    decimalBtn.disabled='true'
-}
+
+
+
+
 
 
 
